a30ca0117ba54de78ff8b8db04d2b46b48ae2158
Wd <13335462685@163.com>
2022-08-12 19:29:03 +0800

    springcloud-1

---
+++ cloud-config-client-3355/src/main/java/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")	//spring的@Value注解
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
